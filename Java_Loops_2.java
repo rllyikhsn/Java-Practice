929f1609d5b8c538ee3b6343d8cc529b47473d22
rllyikhsn <angledark68@gmail.com>
2019-03-31 12:01:33 +0700

    Hacker Rank Java

---
+++ Java_Loops_2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+/**
+ * Java_Loops_2
+ */
+public class Java_Loops_2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int i = 0; i<=t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int seq = a + (int)Math.pow(2, 0) * b;
+            System.out.print(seq + " ");
+            for (int j = 1;j<=n;j++){
+                seq = seq + (int)Math.pow(2, j) * b;
+                System.out.print(seq + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
